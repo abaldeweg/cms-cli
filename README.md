06f1bbefa29428959716a902c21cca4b1349a943
André Baldeweg <kontakt@andrebaldeweg.de>
2020-10-14 12:39:51 +0200

    feature: baldeweg/cms-cli

---
+++ README.md
@@ -0,0 +1,12 @@
+# baldeweg/cms-cli
+
+The CLI for the [baldeweg/cms-businesscard](https://github.com/abaldeweg/cms-businesscard) CMS.
+
+## Requirements
+
+- NodeJS 12 <https://nodejs.org/en/>
+- Yarn <https://classic.yarnpkg.com/lang/en/>
+
+## Getting Started
+
+Install it with `yarn add @baldeweg/cms-cli --save`.
---
+++ index.js
@@ -0,0 +1,30 @@
+const cli = require('cac')()
+const { dev, build } = require('@vuepress/core')
+const path = require('path')
+
+cli
+  .command('dev', 'Start the development environment.')
+  .action(() => {
+    dev({
+      sourceDir: path.resolve('content'),
+      theme: '@baldeweg/vuepress-theme-cms',
+      host: 'localhost',
+      open: true,
+      clearScreen: true
+    })
+  })
+
+cli
+  .command('build', 'Build the app.')
+  .action(() => {
+    build({
+      sourceDir: path.resolve('content'),
+      theme: '@baldeweg/vuepress-theme-cms',
+      dest: 'dist',
+      clearScreen: true
+    })
+  })
+
+
+cli.help()
+cli.parse()
